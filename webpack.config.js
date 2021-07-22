3e360ba1467fcf17d9d6520dc9451cf1c1c9a6bb
Yoshihiro Tanaka <contact@cordea.jp>
2021-07-22 11:51:34 +0900

    install material-components

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,23 @@ module.exports = {
                     esModule: true,
                 },
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                            webpackImporter: false,
+                            sassOptions: {
+                                includePaths: ['./node_modules'],
+                            },
+                        },
+                    },
+                ],
+            },
         ],
     },
     resolve: {
