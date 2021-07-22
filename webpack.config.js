0bef0e119c43546502b851a9bef2ace0fbaf4297
Yoshihiro Tanaka <contact@cordea.jp>
2021-07-22 10:26:25 +0900

    compile vue components

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,40 @@
 const path = require('path');
+const {VueLoaderPlugin} = require('vue-loader')
 
 module.exports = {
     mode: 'production',
     entry: {
         background: './src/background.ts',
+        options: './src/options.ts',
     },
     module: {
         rules: [
             {
                 test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
+                exclude: /node_modules|vue\/src/,
+                loader: 'ts-loader',
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                },
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    esModule: true,
+                },
             },
         ],
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+        },
     },
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'src'),
