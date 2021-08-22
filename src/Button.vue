0feeb3b77179029370b6902b7ff066efc6ed5a70
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-22 15:13:36 +0900

    define button

---
+++ src/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="mdc-button mdc-button--outlined" v-on="$listeners">
+    <span class="mdc-button__ripple"></span>
+    <span class="mdc-button__label">{{ label }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import {MDCRipple} from '@material/ripple';
+
+export default {
+  name: "Button",
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+  },
+  mounted() {
+    new MDCRipple(document.querySelector('.mdc-button'));
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@use "@material/button/styles";
+</style>
