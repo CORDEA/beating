f11756bc9ad6d9eb49a668a675fc6c507f81e3ba
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-22 15:48:05 +0900

    store value

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,51 @@
 <template>
   <div id="app">
+    <TextField hint="Url" :value.sync="url"/>
+    <Slider :value.sync="volume"/>
+    <Button @click="onSubmit"/>
   </div>
 </template>
 
 <script lang="ts">
+import TextField from './TextField.vue';
+import Slider from "./Slider.vue";
+import Button from "./Button.vue";
+
+const key = 'BEATING_VALUES';
+
+export default {
+  components: {
+    Slider,
+    TextField,
+    Button,
+  },
+  data: function () {
+    return {
+      url: '',
+      volume: 0,
+    };
+  },
+  methods: {
+    onSubmit: function () {
+      const url = this.url;
+      const volume = this.volume;
+      chrome.storage.local.get([key], function (map) {
+        const value = {
+          'url': url,
+          'volume': volume,
+        };
+        const object: { [key: string]: any } = {};
+        const values = map[key] ?? [];
+        values.push(value)
+        object[key] = values;
+        chrome.storage.local.set(object, function () {
+        });
+      });
+    }
+  },
+}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
 </style>
