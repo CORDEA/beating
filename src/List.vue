82eb44e29b5898fb34d460defd01aa51cf380d87
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-22 14:56:32 +0900

    define list

---
+++ src/List.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="mdc-list mdc-list--two-line">
+    <li class="mdc-list-item">
+      <span class="mdc-list-item__ripple"></span>
+      <span class="mdc-list-item__text">
+      <span class="mdc-list-item__primary-text">{{ title }}</span>
+      <span class="mdc-list-item__secondary-text">{{ description }}</span>
+    </span>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import {MDCList} from '@material/list';
+
+export default {
+  name: "List",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  mounted() {
+    new MDCList(document.querySelector('.mdc-list'));
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@use "@material/list";
+
+@include list.deprecated-core-styles;
+</style>
