7dbd96bfa7a100819332e6fd03125057d5ae7ae7
Yoshihiro Tanaka <contact@cordea.jp>
2021-11-07 13:40:16 +0900

    make more readable

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,25 +1,24 @@
 import {KEY} from './constants';
 import {PageVolume} from './PageVolume';
 
-function updateVolume() {
-  const url = location.href;
-  chrome.storage.local.get(KEY, async function (map) {
-    const matches = map[KEY].filter((page: PageVolume) => url.startsWith(page.url));
-    if (matches.length <= 0) {
-      return;
-    }
-    const first = matches[0];
-    document
-      .querySelectorAll('audio')
-      .forEach((e) => {
-        e.volume = first.volume;
-      });
-    document
-      .querySelectorAll('video')
-      .forEach((e) => {
-        e.volume = first.volume;
-      });
-  });
+function updateVolume(volume: number) {
+  document
+    .querySelectorAll('audio')
+    .forEach((e) => {
+      e.volume = volume;
+    });
+  document
+    .querySelectorAll('video')
+    .forEach((e) => {
+      e.volume = volume;
+    });
 }
 
-updateVolume();
+const url = location.href;
+chrome.storage.local.get(KEY, async function (map) {
+  const matches = map[KEY].filter((page: PageVolume) => url.startsWith(page.url));
+  if (matches.length <= 0) {
+    return;
+  }
+  updateVolume(matches[0].volume);
+});
